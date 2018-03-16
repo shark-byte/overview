8be52c63f8415b400e5a72ca8ff317fa6a4f07a1
jordynmcginnis <jordynm4@gmail.com>
2018-03-16 14:58:58 -0700

    made schema

--- seedSql.js
+++ seedSql.js
@@ -1,32 +1,33 @@
 
 
+CREATE DATABASE weGotData;
 
-// CREATE TABLE employees (
-//     emp_no      INT             NOT NULL,  -- UNSIGNED AUTO_INCREMENT??
-//     birth_date  DATE            NOT NULL,
-//     first_name  VARCHAR(14)     NOT NULL,
-//     last_name   VARCHAR(16)     NOT NULL,
-//     gender      ENUM ('M','F')  NOT NULL,  -- Enumeration of either 'M' or 'F'
-//     hire_date   DATE            NOT NULL,
-//     PRIMARY KEY (emp_no)                   -- Index built automatically on primary-key column
-//                                            -- INDEX (first_name)
-//                                            -- INDEX (last_name)
-// );
-
-function test () {
-
-}
-
-test();
+USE weGotData;
 
 
+// { id: 4599956,
+//   name: 'bob',
+//   tagline: 'bad',
+//   type: 'Restaurant',
+//   vicinity: '495 Geary Street, San Francisco',
+//   priceLevel: 3,
+//   zagatFood: 3.5,
+//   zagatDecor: 9.3,
+//   zagatService: 3,
+//   longDescription: 'So ullamco tongue minim commodo do meatball tail dru' }
 
 
-
-
-
-
-
-
+CREATE TABLE restaurant (
+  id int auto_increment,
+  name varchar(10),
+  tagline varchar(15),
+  _type varchar(12),
+  vicinity varchar(50),
+  priceLevel: int,
+  zagatFood: int,
+  zagatDecor: int,
+  zagatService: int,
+  longDescription: varchar(100)
+);
 
 
